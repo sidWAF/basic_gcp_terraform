671832ae56449c412d2e8ca34cf0956ca1bf0181
sidWAF <sidoaws1@gmail.com>
2024-04-23 21:49:15 -0400

    adding files to gcp terraform

---
+++ main.tf
@@ -0,0 +1,111 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+project = "sidnewproject-384215"
+region = "us-east-1"
+#zone = us-east-1a
+credentials = "sidnewproject-384215-63b46cc71de8.json"
+}
+
+
+
+resource "google_storage_bucket" "bucket" {
+  name          = "itaque1"
+  location      = "EU"
+  force_destroy = true
+
+  # uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "error.html"
+  }
+
+  uniform_bucket_level_access = false
+}
+
+// Setting the bucket ACL to public read
+resource "google_storage_bucket_acl" "bucket_acl" {
+  bucket         = google_storage_bucket.bucket.name
+  predefined_acl = "publicRead"
+}
+
+// Uploading and setting public read access for HTML files
+resource "google_storage_bucket_object" "upload_html" {
+  for_each     = fileset("${path.module}/", "*.html")
+  bucket       = google_storage_bucket.bucket.name
+  name         = each.value
+  source       = "${path.module}/${each.value}"   #name, source, and content_type arguements are metadata. The each.value arguement gets each html fle and content type shows it's text and html
+  content_type = "text/html"
+}
+
+// Public ACL for each HTML file
+resource "google_storage_object_acl" "html_acl" {
+  for_each       = google_storage_bucket_object.upload_html
+  bucket         = google_storage_bucket_object.upload_html[each.key].bucket
+  object         = google_storage_bucket_object.upload_html[each.key].name
+  predefined_acl = "publicRead"
+}
+
+// Uploading and setting public read access for image files
+resource "google_storage_bucket_object" "upload_images" {
+  for_each     = fileset("${path.module}/", "*.jpg")
+  bucket       = google_storage_bucket.bucket.name
+  name         = each.value
+  source       = "${path.module}/${each.value}"
+  content_type = "image/jpeg"
+}
+
+// Public ACL for each image file
+resource "google_storage_object_acl" "image_acl" {
+  for_each       = google_storage_bucket_object.upload_images
+  bucket         = google_storage_bucket_object.upload_images[each.key].bucket
+  object         = google_storage_bucket_object.upload_images[each.key].name
+  predefined_acl = "publicRead"
+}
+
+output "website_url" {
+  value = "https://storage.googleapis.com/${google_storage_bucket.bucket.name}/index.html"
+}
+
+#   cors {
+#     origin          = ["http://image-store.com"]
+#     method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+#     response_header = ["*"]
+#     max_age_seconds = 3600
+#   }
+# }
+
+#life cycle
+
+# resource "google_storage_bucket" "itaque-bucket-2" {
+#   name          ="itaque1-store"
+#   location      = "US"
+#   force_destroy = true
+
+#   lifecycle_rule {
+#     condition {
+#       age = 3
+#     }
+#     action {
+#       type = "Delete"
+#     }
+#   }
+
+#   lifecycle_rule {
+#     condition {
+#       age = 1
+#     }
+#     action {
+#       type = "AbortIncompleteMultipartUpload"
+#     }
+#   }
+# }
