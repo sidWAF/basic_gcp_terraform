1ef0384ef97dc8ca9ee2c0502ffae6124dd5fcfa
sidWAF <sidoaws1@gmail.com>
2024-04-26 23:05:52 -0400

    my auto-vpc

--- main.tf
+++ main.tf
@@ -109,3 +109,30 @@ output "website_url" {
 #     }
 #   }
 # }
+
+
+resource "google_compute_network" "auto-vpc-tf" {
+  name = "auto-vpc-tf"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "itaque-sg" {
+  name ="itaque-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.32.1.0/24"
+  region = "asia-northeast1"
+} 
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
